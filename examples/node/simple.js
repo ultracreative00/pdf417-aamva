6ca6b31b0d2a6fe87e268bb58f91a58e38fb927b
ultracreative00 <gebbabobby@gmail.com>
2026-05-23 23:00:19 -0700

    Fix: sync all missing and mismatched files from pkoretic/pdf417-generator

--- examples/node/simple.js
+++ examples/node/simple.js
@@ -1,26 +1,27 @@
 const { createCanvas } = require("canvas")
 const { PDF417, HUB3 } = require("../../lib/pdf417")
+const fs = require("fs")
 
 const code = HUB3.format({
-    amount: 12355,
-    sender: {
-        name: "PETAR KORETIĆ",
-        street: "PREVOJ DD",
-        city: "10000 Zagreb"
-    },
-    receiver: {
-        name: "FIRMA J.D.O.O",
-        street: "PREVOJ DD",
-        city: "10000 ZAGREB",
-        iban: "HR1210010051863000160",
-        model: "HR01",
-        reference: "7336-68949637625-00001"
-    },
-    purpose: "COST",
-    description: "Uplata za 1. mjesec"
+    amount:        123.55,
+    payerName:     "PETAR KORETIĆ",
+    payerAddress:  "PREVOJ DD",
+    payerCity:     "10000 Zagreb",
+    recipientName: "FIRMA J.D.O.O",
+    recipientAddr: "PREVOJ DD",
+    recipientCity: "10000 ZAGREB",
+    iban:          "HR5041240000000000000",
+    model:         "HR01",
+    callNumber:    "7336-68949637625-00001",
+    purposeCode:   "COST",
+    description:   "Uplata za 1. mjesec"
 })
 
-const canvas = createCanvas(1, 1)
+let canvas = createCanvas(1, 1)
 PDF417.draw(code, canvas)
 
+// create image which can be sent in an e-mail or similar
 console.log(`<img src="${canvas.toDataURL()}" />`)
+
+// save as PNG file
+fs.writeFileSync("barcode.png", canvas.toBuffer("image/png"))
